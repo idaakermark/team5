1acabd6f540d72ee92dd8d37c94b2265c3549016
idamakela <idamakela98@gmail.com>
2023-08-23 11:18:52 +0200

    ordered the respons so data obeject return newest to oldest comment

--- src/api/comments.js
+++ src/api/comments.js
@@ -1,10 +1,9 @@
-import {supabase} from '../supabaseClient.js';
+import { supabase } from '../supabaseClient.js';
 
 export const commentsCacheKey = '/api/comments';
 
 //GET all comments
 export const getComments = async () => {
-  const { data, error, status } = await supabase.from('comments').select('*');
-
+  const { data, error, status } = await supabase.from('comments').select('*').order('created_at', { ascending: false });
   return { data, error, status };
 };
