2fad1c1b2d05956dac3572d99f6171a52dbd0344
idaakermark <ida.akermark@edu.cmeducations.se>
2023-08-23 12:13:04 +0200

    Add copyright to footer

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -8,7 +8,8 @@ export default function Footer() {
       <div className={styles.footerContainer}>
         <div className={styles.aboutContainer}>
             <h3>About</h3>
-            <p>Welcome to the Never-Ending Comment Page, where conversations stretch to infinity. Join our community and be part of an endless exchange of ideas and perspectives.</p>
+            <p className={styles.aboutText}>Welcome to the Never-Ending Comment Page, where conversations stretch to infinity. Join our community and be part of an endless exchange of ideas and perspectives.</p>
+            <p className={styles.copyright}>© 2023 The Never-Ending Comment Page. All rights reserved.</p>
         </div>
         <div className={styles.logoContainer}>
           <img src={logo} alt="Logo" className={styles.logo}/>
