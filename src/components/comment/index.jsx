b36512319ea0df3b90277f66aea43b7b6c880a47
idamakela <idamakela98@gmail.com>
2023-08-23 13:18:20 +0200

    error while getting data through react component comment and props

--- src/components/comment/index.jsx
+++ src/components/comment/index.jsx
@@ -1,14 +1,16 @@
 export default function Comment({ id, created_at, alias, comment }) {
+    console.log("comment data:" + { id, created_at, alias, comment });
 
   return (
-    <div key={comment.id} className={styles.comment}>
-      <h6># {index + 1}</h6>
-      <p>{comment}</p>
-      <div className={styles.authorContainer}>
-        Author:
-        <span className={styles.author}> {author}</span>
-        <span className={styles.date}> {date}</span>
-      </div>
-    </div>
-  );
+    <div> HI</div>
+    // <div key={comment.id} className={styles.comment}>
+    //   <h6># {index + 1}</h6>
+    //   <p>{comment}</p>
+    //   <div className={styles.authorContainer}>
+    //     Author:
+    //     <span className={styles.author}> {author}</span>
+    //     <span className={styles.date}> {date}</span>
+    //   </div>
+    // </div>
+  )
 }
--- src/components/main/index.jsx
+++ src/components/main/index.jsx
@@ -2,33 +2,38 @@ import React from 'react';
 import styles from './main.module.css';
 import useSWR from 'swr';
 import { getComments, commentsCacheKey } from '../../api/comments.js';
+import Comment from '../comment/index.jsx';
 
 export default function Main() {
   //GET all comments
   const { data: { data = [] } = {} } = useSWR(commentsCacheKey, getComments);
   console.log(data);
+  console.log(data[0]);
 
-  const author = "Jane Doe"
-  const date = "August 22, 2023 at 14:35"
+  const author = 'Jane Doe';
+  const date = 'August 22, 2023 at 14:35';
   const comments = [
     "Wow, this website's design is incredibly user-friendly and visually appealing. Kudos to the designers!",
     "I'm impressed with how easy it is to navigate this site. Great job!",
-    "The color scheme here is fantastic, it really adds to the overall experience.",
+    'The color scheme here is fantastic, it really adds to the overall experience.',
     "I love the attention to detail in the layout. It's clear that a lot of thought went into it.",
-    "This website sets a high bar for usability. Well done!",
+    'This website sets a high bar for usability. Well done!',
     "I've seen a lot of websites, and this one stands out for all the right reasons.",
-    "The design is so clean and intuitive. It makes browsing a pleasure.",
-    "The designers nailed it! This site is a joy to use.",
-    "I wish more websites were as well-designed as this one.",
-    "I can't stop admiring how polished and user-friendly this website is."
+    'The design is so clean and intuitive. It makes browsing a pleasure.',
+    'The designers nailed it! This site is a joy to use.',
+    'I wish more websites were as well-designed as this one.',
+    "I can't stop admiring how polished and user-friendly this website is.",
   ];
 
   return (
     <div className={styles.mainContent}>
       <div className={styles.commentContainer}>
-      <h1 className={styles.rubrik}>Comments</h1>
+        <h1 className={styles.rubrik}>Comments</h1>
+        {data.map((comment) => {
+          <Comment key={comment.id} {...comment} />;
+        })}
         {/* Example comment content */}
-        {comments.map((comment, index) => (
+        {/* {comments.map((comment, index) => (
           <div key={index} className={styles.comment}>
             <h6># {index + 1}</h6>
             <p>{comment}</p>
@@ -37,7 +42,7 @@ export default function Main() {
               <span className={styles.date}> {date}</span>
             </div>
           </div>
-        ))}
+        ))} */}
       </div>
     </div>
   );
