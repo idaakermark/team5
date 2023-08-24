a29ca7fe0176554baeac6735017b4c9d04e9fdcd
idaakermark <ida.akermark@edu.cmeducations.se>
2023-08-24 13:53:21 +0200

    Delete unnecessary code

--- src/api/comments.js
+++ src/api/comments.js
@@ -1,31 +1,8 @@
-// import { supabase } from '../supabaseClient.js';
-
-// export const commentsCacheKey = '/api/comments';
-
-// //GET all comments
-// export const getComments = async () => {
-//   const { data, error, status } = await supabase.from('comments').select('*').order('created_at', { ascending: false });
-
-//   return { data, error, status };
-// };
-
-// //ADD comments
-// export const addComment = async (_, { arg: commentData }) => {
-//   const { data, error } = await supabase
-//     .from('comments')
-//     .insert({ ...commentData })
-//     .single()
-//     .select("*");
-
-//   return { error, data };
-// };
-
-
 import { supabase } from '../supabaseClient.js';
 
 export const commentsCacheKey = '/api/comments';
 
-// Function to fetch comments from the database
+// GET all comments
 export const getComments = async () => {
   const cachedComments = localStorage.getItem(commentsCacheKey);
   
