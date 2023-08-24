58f2db7c3c8e37640a96f0d74cdc9fd53f8dab2b
idaakermark <ida.akermark@edu.cmeducations.se>
2023-08-24 13:32:57 +0200

    feature T/28, The user should be able to see the comments even if the database is paused.

--- src/api/comments.js
+++ src/api/comments.js
@@ -1,21 +1,64 @@
+// import { supabase } from '../supabaseClient.js';
+
+// export const commentsCacheKey = '/api/comments';
+
+// //GET all comments
+// export const getComments = async () => {
+//   const { data, error, status } = await supabase.from('comments').select('*').order('created_at', { ascending: false });
+
+//   return { data, error, status };
+// };
+
+// //ADD comments
+// export const addComment = async (_, { arg: commentData }) => {
+//   const { data, error } = await supabase
+//     .from('comments')
+//     .insert({ ...commentData })
+//     .single()
+//     .select("*");
+
+//   return { error, data };
+// };
+
+
 import { supabase } from '../supabaseClient.js';
 
 export const commentsCacheKey = '/api/comments';
 
-//GET all comments
+// Function to fetch comments from the database
 export const getComments = async () => {
+  const cachedComments = localStorage.getItem(commentsCacheKey);
+  
+  if (cachedComments) {
+    return { data: JSON.parse(cachedComments), error: null };
+  }
+
   const { data, error, status } = await supabase.from('comments').select('*').order('created_at', { ascending: false });
+
+  if (!error && data) {
+    localStorage.setItem(commentsCacheKey, JSON.stringify(data));
+  }
 
   return { data, error, status };
 };
 
-//ADD comments
+// //ADD comments
 export const addComment = async (_, { arg: commentData }) => {
   const { data, error } = await supabase
     .from('comments')
     .insert({ ...commentData })
     .single()
     .select("*");
+
+
+  if (!error && data) {
+    const cachedComments = localStorage.getItem(commentsCacheKey);
+    if (cachedComments) {
+      const updatedCachedComments = JSON.parse(cachedComments);
+      updatedCachedComments.unshift(data);
+      localStorage.setItem(commentsCacheKey, JSON.stringify(updatedCachedComments));
+    }
+  }
 
   return { error, data };
 };
