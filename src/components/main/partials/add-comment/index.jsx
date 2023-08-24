297ea59eb81df2c3d683d398e071bc171a18b44c
idamakela <idamakela98@gmail.com>
2023-08-24 17:27:22 +0200

    added maxlength to textarea to limit charachter input from user

--- src/components/main/partials/add-comment/index.jsx
+++ src/components/main/partials/add-comment/index.jsx
@@ -1,12 +1,12 @@
-import { useRef } from "react";
-import styles from "./add-comment.module.css";
-import useSWRMutation from "swr/mutation";
+import { useRef } from 'react';
+import styles from './add-comment.module.css';
+import useSWRMutation from 'swr/mutation';
 import { addComment, commentsCacheKey } from '../../../../api/comments.js';
-import { useState } from "react";
+import { useState } from 'react';
 
 export default function AddComment() {
   const formRef = useRef();
-  const [isFormHidden, setIsFormHidden] = useState(true); 
+  const [isFormHidden, setIsFormHidden] = useState(true);
 
   const { trigger: addTrigger } = useSWRMutation(commentsCacheKey, addComment);
 
@@ -33,33 +33,35 @@ export default function AddComment() {
 
   return (
     <div className={styles.commentFrame}>
-        {isFormHidden ? (
-          <button className={styles.addCommentButton} onClick={toggleFormVisibility}>
-            Add a comment
-          </button>
-        ) : (
-          <div className={styles.commentContainer}>
+      {isFormHidden ? (
+        <button
+          className={styles.addCommentButton}
+          onClick={toggleFormVisibility}
+        >
+          Add a comment
+        </button>
+      ) : (
+        <div className={styles.commentContainer}>
           <form ref={formRef} className={styles.form} onSubmit={handleOnSubmit}>
             <div className={styles.inputContainer}>
-              <h5 htmlFor="alias">Author</h5>
-              <input id="alias" name="alias"></input>
+              <h5 htmlFor='alias'>Author</h5>
+              <input id='alias' name='alias'></input>
             </div>
 
             <div className={styles.inputContainer}>
-              <h5 htmlFor="comment">Comment</h5>
-              <textarea id="comment" name="comment"></textarea>
+              <h5 htmlFor='comment'>Comment</h5>
+              <textarea id='comment' name='comment' maxlength='305'></textarea>
             </div>
 
-            <button className={styles.button} type="submit">
+            <button className={styles.button} type='submit'>
               Submit
             </button>
             <button className={styles.closeForm} onClick={toggleFormVisibility}>
               close
             </button>
           </form>
-          </div>
-        )}
-
+        </div>
+      )}
     </div>
   );
 }
