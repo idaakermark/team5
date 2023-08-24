c36bb6f3db3f1bdb04e00caa4b652f31b06f7fb2
Ida Mäkelä <116893968+idamakela@users.noreply.github.com>
2023-08-24 10:25:29 +0200

    Feature/t15: Code review (#26), merge to develop
    * ordered the respons so data obeject return newest to oldest comment
    
    * made component file/function for displaying comments
    
    * changed name on component for semantic reasons, and included correct props
    
    * error while getting data through react component comment and props
    
    * comments from database is displayed on frontend now, styling is the same for the component, only moved it for easier overview on code

---
+++ src/components/comment/index.jsx
@@ -0,0 +1,23 @@
+import styles from '../main/main.module.css';
+
+export default function Comment({
+  id,
+  created_at,
+  alias,
+  comment,
+  index,
+  dataLenght,
+}) {
+
+  return (
+    <div key={id} className={styles.comment}>
+      <h6># {dataLenght - index}</h6>
+      <p>{comment}</p>
+      <div className={styles.authorContainer}>
+        Author:
+        <span className={styles.author}> {alias}</span>
+        <span className={styles.date}> {created_at}</span>
+      </div>
+    </div>
+  );
+}
